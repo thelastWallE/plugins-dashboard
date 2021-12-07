115a18dfb9477fd054050162787d3589c0a14f4f
thelastWallE <12502210+thelastWallE@users.noreply.github.com>
2021-12-07 09:44:48 +0100

    change default variablename

--- src/components/plugin-tile.js
+++ src/components/plugin-tile.js
@@ -71,7 +71,7 @@ class PluginTile extends React.Component {
       var ci = i % baseColors.length;
       versionColors[versionData[i].version] = baseColors[ci];
     }
-    var str = JSON.stringify(obj, null, 4);
+    var str = JSON.stringify(versionData, null, 4);
     console.log(str);
     return (
       <Paper className={classes.paper} >
